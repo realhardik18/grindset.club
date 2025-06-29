3f950a0b858666e62e1e33c4671a99c2af63dd09
realhardik18 <realhardik18@gmail.com>
2025-06-30 05:26:38 +0530

    setting up clerk rn

--- app/layout.js
+++ app/layout.js
@@ -1,5 +1,6 @@
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
+import { ClerkProvider } from "@clerk/nextjs";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -18,11 +19,13 @@ export const metadata = {
 
 export default function RootLayout({ children }) {
   return (
-    <html lang="en">
+    <html lang="en">      
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
+        <ClerkProvider>
         {children}
+        </ClerkProvider>        
       </body>
     </html>
   );
